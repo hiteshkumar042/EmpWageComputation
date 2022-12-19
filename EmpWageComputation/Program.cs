816dbdd8cbf587b34655f3137e6361cbb78cafdd
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-19 13:00:22 +0530

    Modifying to impletement while loop

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -4,47 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation!");
+            Console.WriteLine("Welcome to EmplyeeWageCompute");
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
             const int MAX_WORKING_DAYS = 20;
-            int empHrs = 0;
-            int empWage = 0;
-            int totalWage = 0;
-            int day;
+            const int MAX_WORKING_HRS = 80;
+            int empHrs = 0, empWage = 0, totalWage = 0, day = 1, totalHrs = 0;
 
             //UC-1 Check employee present or not
             Random random = new Random();
-            for ( day=1; day <= MAX_WORKING_DAYS; day++)
+
+            while (day <= MAX_WORKING_DAYS && totalHrs <= MAX_WORKING_HRS)
             {
                 int employeeInput = random.Next(0, 3);
-                //UC4 using Switch case
+                //UC-4- Switch case
                 switch (employeeInput)
                 {
-                    case 1:
-                 //Console.WriteLine("FullTime Employee is Present");
+                    case FULL_TIME:
                         empHrs = 8;
                         break;
-                    case 2:
-
-                     // Console.WriteLine("PartTime Employee is Present");
+                    case PART_TIME:
                         empHrs = 4;
                         break;
                     default:
-                     // Console.WriteLine("Employee is Absent");
                         empHrs = 0;
                         break;
                 }
-                //UC-2 & UC-3 calculate Employee Wage
+                //UC-2 calculate EmployeeWage
                 empWage = EMP_RATE_PER_HR * empHrs;
-                totalWage = totalWage + empWage;
-                Console.WriteLine("Employee wage is :" + empWage);
+                totalWage += empWage; //totalWage=totalWage+empWage
+                totalHrs += empHrs;
+                day++;
             }
-            //UC5 total wage for 20 days
-            Console.WriteLine("TotalWage Wage for {0} days:{1}", (day - 1), totalWage);
-
-           
+            Console.WriteLine("Total wage for {0} days:{1} and Hrs:{2}", (day - 1), totalWage, (totalHrs - empHrs));
+            Console.ReadLine();
         }
     }
 }
