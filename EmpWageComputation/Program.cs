fc9d3ec3660a52dd01f8deee03f4849a9867f390
hiteshkumar794 <hiteshkumar794@gmail.com>
2022-12-19 12:47:11 +0530

    For Loop Added for UC 5

--- EmpWageComputation/Program.cs
+++ EmpWageComputation/Program.cs
@@ -8,32 +8,43 @@
             const int FULL_TIME = 1;
             const int PART_TIME = 2;
             const int EMP_RATE_PER_HR = 20;
+            const int MAX_WORKING_DAYS = 20;
             int empHrs = 0;
             int empWage = 0;
+            int totalWage = 0;
+            int day;
 
             //UC-1 Check employee present or not
             Random random = new Random();
-            int employeeInput = random.Next(0, 3);
-            switch (employeeInput) //UC4 using Switch case
+            for ( day=1; day <= MAX_WORKING_DAYS; day++)
             {
-                case 1:
-                    Console.WriteLine("FullTime Employee is Present");
-                    empHrs = 8;
-                    break;
-                case 2:
+                int employeeInput = random.Next(0, 3);
+                //UC4 using Switch case
+                switch (employeeInput)
+                {
+                    case 1:
+                 //Console.WriteLine("FullTime Employee is Present");
+                        empHrs = 8;
+                        break;
+                    case 2:
 
-                    Console.WriteLine("PartTime Employee is Present");
-                    empHrs = 4;
-                    break;
-                default:
-                    Console.WriteLine("Employee is Absent");
-                    empHrs = 0;
-                    break;
+                     // Console.WriteLine("PartTime Employee is Present");
+                        empHrs = 4;
+                        break;
+                    default:
+                     // Console.WriteLine("Employee is Absent");
+                        empHrs = 0;
+                        break;
+                }
+                //UC-2 & UC-3 calculate Employee Wage
+                empWage = EMP_RATE_PER_HR * empHrs;
+                totalWage = totalWage + empWage;
+                Console.WriteLine("Employee wage is :" + empWage);
             }
+            //UC5 total wage for 20 days
+            Console.WriteLine("TotalWage Wage for {0} days:{1}", (day - 1), totalWage);
 
-            //UC-2 & UC-3 calculate Employee Wage
-            empWage = EMP_RATE_PER_HR*empHrs;
-            Console.WriteLine("Employee wage is :" + empWage);
+           
         }
     }
 }
